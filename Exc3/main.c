d0f560d4f764bc895f9315f09eeb4406d6c9e135
Nadman Faisal <nadman@chalmers.se>
2025-02-03 21:31:15 +0100

    Append to file, and clear buffer

--- Exc3/main.c
+++ Exc3/main.c
@@ -15,15 +15,43 @@ void search_by_firstname(char *name);           // Prints out the person if
                                                 // in list
 void append_file(PERSON *inrecord);             // appends a new person to the file
 void printMenu();
+void clearBuffer();
 
 int main(int argc, char *argv[]) {
+    int user_input;
     printMenu();
     printf("\n");
 
     PERSON ppost = {"nadman", "faisal", "20040920461c"};
+    PERSON ppost2 = {"zahid", "hasan", "20001231123c"};
+    PERSON ppost3 = {"Mohd", "Taha", "20040404123c"};
 
-    write_new_file(&ppost);
+    printf("What would you like to do?: ");
+    scanf("%d", &user_input);
+    clearBuffer();
+
+    if(user_input == 1) {
+        printf("Create a new file!\n");
+        write_new_file(&ppost);
+    } else if(user_input == 2) {
+        printf("Write to a file!\n");
+        append_file(&ppost2);
+    } else {
+        printf("Invalid input. Please try again: \n");
+    }
+
     return (0);
+}
+
+/*
+The buffer is cleared each time an input is taken from the user.
+This method functions by retrieving the charactes and discarding them
+until it reaches a new-line character OR it reaches the end of file.
+*/
+
+void clearBuffer(void) {
+    char input;
+    while((input = getchar()) != '\n' && input != EOF);
 }
 
 void printMenu() {
@@ -41,3 +69,13 @@ void write_new_file(PERSON *inrecord) {
     fclose(file);
 }
 
+void append_file(PERSON *inrecord) {
+    PERSON *temp = inrecord;
+    FILE* file = fopen("peoplesDb.bin", "wb");
+    if (file == NULL) {
+        perror("Error opening file");
+        return;
+    }
+    fwrite(temp, sizeof(PERSON), 1, file);
+    fclose(file);
+}
