9d80102d574b499cfd080da0e2821a1de4708a00
Nadman Faisal <nadman@chalmers.se>
2025-02-06 11:10:06 +0100

    Handle invalid arguements

--- Exc4/main.c
+++ Exc4/main.c
@@ -1,1 +1,10 @@
+#include <stdio.h>
 
+int main(int argc, char *argv[]) {
+    if(argc < 6 || argc > 6) {
+        printf("Invalid arguements provided.\n");
+        return 0;
+    }
+    printf("Hello world!\n");
+    return 0;
+}
