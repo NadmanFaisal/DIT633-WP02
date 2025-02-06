98620876d5df64919db968b97e616182dba34385
Nadman Faisal <nadman@chalmers.se>
2025-02-06 11:16:36 +0100

    Handle invalid arguement type

--- Exc4/main.c
+++ Exc4/main.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
+#include <ctype.h>
 
 int main(int argc, char *argv[]) {
     if(argc < 6 || argc > 6) {
         printf("Invalid arguements provided.\n");
         return 0;
     }
+
+    for(int i = 1; i < argc; i++) {
+        if(!isdigit(*argv[i])) {
+            printf("Invalid arguement type. Please provide valid arguements.\n");
+            return 0;
+        }
+    }
+
     printf("Hello world!\n");
     return 0;
 }
